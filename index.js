8b91a57a899cf9da09be1b644aa70e69139ec836
Neha Yadav <nehayadav0102@gmail.com>
2019-09-29 11:33:49 +0530

    import required dependencies

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-const config = require('config')
-const parse = require('ParseUrlData')
+const config = require('./config')
+const parse = require('./utils/parseUrlData')
 const db = require('./utils/dbMethods')
+const promisifyRequest = require('./utils/promisifyRequest')
+const processConcurrentRequest = require('./utils/processConcurrentRequest')
+
 const requestPromise = promisifyRequest(config.crawlUrl)
 
 db.connect(config)
